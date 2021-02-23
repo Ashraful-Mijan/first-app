75a3a5694347ad3ce867ed1f511c1a005e1b8eae
Ashraful-Mijan <ashrafulmijan5@gmail.com>
2021-02-23 13:21:15 +0600

    third-commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
+import React, { useEffect, useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   const products = [
-    {name:'Photoshop', price: '$90.99'},
-    {name:'illustrator', price:'$10.120'}
+    { name: 'Photoshop', price: '$90.99' },
+    { name: 'illustrator', price: '$10.120' },
+    { name: 'PdF Reader', price: '$50.330' }
   ]
+
   return (
     <div className="App">
       <header className="App-header">
-      <Products name={products[0].name} price={products[0].price} />
+        <User></User>
+        <Counter></Counter>
+        {
+          products.map(product => <Products name={product} />)
+        }
+        {/* <Products name={products[0]} />
+      <Products name={products[1]} /> */}
+
         {/* <Person name="Shakib" />
         <Person name="Alamin" />
         <Person name="kalamin" /> */}
@@ -18,6 +28,43 @@ function App() {
   );
 }
 
+function User() {
+  const [state, setState] = useState([]);
+ 
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setState(data))
+  })
+  console.log()
+  return (
+    <div>
+      <h3>Dynamic User: {state.length}</h3>
+      <ul>
+        {
+          state.map(user=> <li>{user.username}</li>)
+        }
+      </ul>
+    </div>
+  )
+}
+
+function Counter() {
+  const [state, setState] = useState(10)
+
+  const decreaseButton = () => {
+    if (state > 0) {
+      setState(state - 1)
+    }
+  }
+  return (
+    <div>
+      <h3>Count: {state} </h3>
+      <button onClick={() => setState(state + 1)}>Increase</button>
+      <button onClick={decreaseButton}>Decrease</button>
+    </div>
+  )
+}
 
 function Products(props) {
   const productStyle = {
@@ -26,12 +73,15 @@ function Products(props) {
     backgroundColor: 'lightgray',
     height: '200px',
     width: '200px',
-    float: 'left'
+    float: 'left',
+    margin: '5px'
   }
+
+  const { name, price } = props.name
   return (
     <div style={productStyle}>
-      <h3>{props.name}</h3>
-      <h5>{props.price}</h5>
+      <h3>{name}</h3>
+      <h5>{price}</h5>
       <button>Buy now</button>
     </div>
   )
